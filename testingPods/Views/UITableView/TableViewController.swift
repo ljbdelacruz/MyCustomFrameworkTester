2989529ecd529ec05620986812ba69fde29adfad
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-04-25 08:09:49 +0800

    updates

---
+++ testingPods/Views/UITableView/TableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TableViewController.swift
+//  testingPods
+//
+//  Created by Lainel John Dela Cruz on 24/04/2019.
+//  Copyright © 2019 Lainel John Dela Cruz. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
