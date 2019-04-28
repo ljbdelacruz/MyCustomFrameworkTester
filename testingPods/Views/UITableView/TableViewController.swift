5c2ef38d31e87a720ebd7a0687535fa1c27ecbcb
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-04-28 23:23:40 +0800

    fix scrollview

--- testingPods/Views/UITableView/TableViewController.swift
+++ testingPods/Views/UITableView/TableViewController.swift
@@ -16,6 +16,7 @@ class TableViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.initToolbar()
         self.setupTableCell();
         self.items=UITableViewModelContent.initStubData();
         self.myTV.reloadData();
